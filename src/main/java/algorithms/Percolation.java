f98eae2a8803a94df7ab8c5dca8f06a13f3c356c
beetlewar@mail.ru <beetlewar@mail.ru>
2017-06-16 00:23:05 +0300

    initial commit with empty percolation

---
+++ src/main/java/algorithms/Percolation.java
@@ -0,0 +1,28 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation {
+    public Percolation(int n) {
+    }
+
+    public void open(int row, int col) {
+
+    }
+
+    public boolean isOpen(int row, int col) {
+        return false;
+    }
+
+    public boolean isFull(int row, int col) {
+        return false;
+    }
+
+    public int numberOfOpenSites() {
+        return 0;
+    }
+
+    public boolean percolates() {
+        return false;
+    }
+}
---
+++ src/test/java/algorithms/PercolationTests.java
@@ -0,0 +1,8 @@
+import org.junit.Test;
+
+public class PercolationTests {
+    @Test
+    public void Test(){
+
+    }
+}
