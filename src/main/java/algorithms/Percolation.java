a004f2292f031788d8a617588bf232e003d16b5c
beetlewar@mail.ru <beetlewar@mail.ru>
2017-06-16 01:23:08 +0300

    implemented constructor, open, isOpen, numberOfOpen from Percolation

--- src/main/java/algorithms/Percolation.java
+++ src/main/java/algorithms/Percolation.java
@@ -1,25 +1,66 @@
-import edu.princeton.cs.algs4.StdRandom;
-import edu.princeton.cs.algs4.StdStats;
-import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+package algorithms;
+
+import edu.princeton.cs.algs4.QuickUnionUF;
 
 public class Percolation {
+    private int _n;
+    private boolean[] _openSites;
+    private int _numOpen;
+
     public Percolation(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("'n' must be >= 0.");
+        }
+
+        _n = n;
+
+        int arraySize = n * n;
+        _openSites = new boolean[arraySize];
     }
 
     public void open(int row, int col) {
+        checkRowAndCol(row, col);
 
+        if (isOpen(row, col)) {
+            return;
+        }
+
+        int index = getArrayIndex(row, col);
+
+        _openSites[index] = true;
+        _numOpen++;
+    }
+
+    private int getArrayIndex(int row, int col) {
+        int rowIndex = row - 1;
+        int colIndex = col - 1;
+
+        int index = rowIndex * _n + colIndex;
+        return index;
+    }
+
+    private void checkRowAndCol(int row, int col) {
+        if (row <= 0 || col <= 0 || row > _n || col > _n) {
+            throw new IndexOutOfBoundsException();
+        }
     }
 
     public boolean isOpen(int row, int col) {
-        return false;
+        checkRowAndCol(row, col);
+
+        int index = getArrayIndex(row, col);
+        boolean isOpen = _openSites[index];
+        return isOpen;
     }
 
     public boolean isFull(int row, int col) {
+        checkRowAndCol(row, col);
+
         return false;
     }
 
     public int numberOfOpenSites() {
-        return 0;
+        return _numOpen;
     }
 
     public boolean percolates() {
--- src/test/java/algorithms/PercolationTests.java
+++ src/test/java/algorithms/PercolationTests.java
@@ -1,8 +1,84 @@
+package algorithms;
+
+import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class PercolationTests {
-    @Test
-    public void Test(){
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
 
+    @Test
+    public void Ctor_ZeroOrNegativeN_ThrowsException() {
+        exception.expect(IllegalArgumentException.class);
+
+        new Percolation(0);
+    }
+
+    @Test
+    public void NumberOfOpenSites_NoOpen_ReturnsZero() {
+        Percolation percolation = new Percolation(1);
+
+        int numberOpen = percolation.numberOfOpenSites();
+
+        Assert.assertEquals(0, numberOpen);
+    }
+
+    @Test
+    public void Open_ZeroRow_ThrowsException() {
+        exception.expect(IndexOutOfBoundsException.class);
+
+        Percolation percolation = new Percolation(1);
+        percolation.open(0, 1);
+    }
+
+    @Test
+    public void Open_ZeroCol_ThrowsException() {
+        exception.expect(IndexOutOfBoundsException.class);
+
+        Percolation percolation = new Percolation(1);
+        percolation.open(1, 0);
+    }
+
+    @Test
+    public void Open_LargeRow_ThrowsException() {
+        exception.expect(IndexOutOfBoundsException.class);
+
+        Percolation percolation = new Percolation(1);
+        percolation.open(2, 1);
+    }
+
+    @Test
+    public void Open_LargeCol_ThrowsException() {
+        exception.expect(IndexOutOfBoundsException.class);
+
+        Percolation percolation = new Percolation(1);
+        percolation.open(1, 2);
+    }
+
+    @Test
+    public void Open_IsOpenReturnsOpened() {
+        Percolation percolation = new Percolation(3);
+
+        percolation.open(3, 2);
+
+        boolean isOpen = percolation.isOpen(3, 2);
+
+        Assert.assertTrue(isOpen);
+    }
+
+    @Test
+    public void NumberOfOpenSites_OpenFewSites_ReturnsExpectedNumber() {
+        Percolation percolation = new Percolation(2);
+
+        percolation.open(1, 1);
+        percolation.open(1, 2);
+        percolation.open(2, 1);
+        percolation.open(2, 1);
+
+        int numberOfOpenSites = percolation.numberOfOpenSites();
+
+        Assert.assertEquals(3, numberOfOpenSites);
     }
 }
