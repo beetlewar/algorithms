be08caf05e4b71112bddf6caf186fb3d3a9280ab
beetlewar <beetlewar@mail.ru>
2017-06-28 12:59:42 +0300

    remake brute

--- src/main/java/algorithms/collinear/BruteCollinearPoints.java
+++ src/main/java/algorithms/collinear/BruteCollinearPoints.java
@@ -1,5 +1,6 @@
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
+import java.util.List;
 
 public class BruteCollinearPoints {
     private final LineSegment[] _segments;
@@ -7,128 +8,92 @@ public class BruteCollinearPoints {
     public BruteCollinearPoints(Point[] points) {
         checkPoints(points);
 
-        Point[] pointsCopy = Arrays.copyOf(points, points.length);
+        Arrays.sort(points);
 
-        LinkedList<LineSegment> lineSegments = new LinkedList<LineSegment>();
+        List<Line> lines = new ArrayList<Line>();
 
-        for (Point point : pointsCopy) {
-            for (Point slopingPoint : pointsCopy) {
-                if (slopingPoint == point) {
-                    // don't observe current point
-                    continue;
-                }
-                LineSegment lineSegment = linkPoints(pointsCopy, point, slopingPoint);
-
-                if(lineSegment != null){
-                    lineSegments.addItem(lineSegment);
+        for (int i = 0; i < points.length - 1; i++) {
+            for (int j = i + 1; j < points.length; j++) {
+                Line line = getLine(points, i, j, lines);
+                if (line != null) {
+                    lines.add(line);
                 }
             }
         }
 
-        _segments = LineSegmentsToArray(lineSegments);
+        _segments = CreateLineSegments(lines);
+    }
+
+    private Line getLine(Point[] points, int startIndex, int nextIndex, Iterable<Line> lines) {
+        Point startPoint = points[startIndex];
+        Point nextPoint = points[nextIndex];
+
+        double slope = startPoint.slopeTo(nextPoint);
+
+        Point lastPoint = null;
+        int numSlopePoints = 2;
+
+        for (int i = startIndex + 2; i < points.length; i++) {
+            Point testPoint = points[i];
+            double testSlope = startPoint.slopeTo(testPoint);
+
+            if (Double.compare(slope, testSlope) == 0) {
+                lastPoint = testPoint;
+                numSlopePoints++;
+            }
+        }
+
+        if (numSlopePoints < 4) {
+            return null;
+        }
+
+        if (hasLine(lines, lastPoint, slope)) {
+            return null;
+        }
+
+        return new Line(startPoint, lastPoint, slope);
+    }
+
+    private LineSegment[] CreateLineSegments(List<Line> lines) {
+        LineSegment[] segments = new LineSegment[lines.size()];
+
+        for (int i = 0; i < lines.size(); i++) {
+            Line line = lines.get(i);
+
+            segments[i] = new LineSegment(line.FirstPoint, line.LastPoint);
+        }
+
+        return segments;
+    }
+
+    private boolean hasLine(
+            Iterable<Line> lines,
+            Point lastPoint,
+            double slope) {
+        for (Line line : lines) {
+            if (line.LastPoint == lastPoint && Double.compare(line.Slope, slope) == 0) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     private void checkPoints(Point[] points) {
-        if(points == null)
+        if (points == null)
             throw new IllegalArgumentException();
 
-        for (Point p : points){
-            if( p == null)
+        for (Point p : points) {
+            if (p == null)
                 throw new IllegalArgumentException();
         }
 
         Arrays.sort(points);
 
-        for(int i = 0; i < points.length - 1; i++) {
+        for (int i = 0; i < points.length - 1; i++) {
             if (points[i].compareTo(points[i + 1]) == 0)
                 throw new IllegalArgumentException();
         }
-    }
-
-    private LineSegment[] LineSegmentsToArray(LinkedList<LineSegment> lineSegments) {
-        // copy to array (generic toArray doesn't work)
-        Object[] arr = lineSegments.toArray();
-
-        LineSegment[] segments = new LineSegment[arr.length];
-
-        int numDistinct = 0;
-
-        for(int i = 0; i < segments.length; i++){
-            LineSegment newSegment = (LineSegment)arr[i];
-            boolean hasSegment = false;
-            // search distinct segments
-            for(int j = 0; j < i; j++){
-                LineSegment distinctSegment = (LineSegment)arr[j];
-                if(newSegment.toString().equals(distinctSegment.toString())){
-                    hasSegment = true;
-                    break;
-                }
-            }
-
-            if(!hasSegment) {
-                numDistinct++;
-                segments[i] = newSegment;
-            }
-        }
-
-        LineSegment[] distinctSegments = new LineSegment[numDistinct];
-        int index = 0;
-        for(int i = 0; i < segments.length; i++){
-            LineSegment segment = segments[i];
-            if(segment != null){
-                distinctSegments[index] = segment;
-                index++;
-            }
-        }
-
-        return distinctSegments;
-    }
-
-    private LineSegment linkPoints(Point[] points, Point startPoint, Point slopingPoint) {
-        Comparator<Point> comparator = startPoint.slopeOrder();
-
-        LinkedList linkedPoints = new LinkedList();
-        linkedPoints.addItem(startPoint);
-        linkedPoints.addItem(slopingPoint);
-
-        for (Point slopingPoint2 : points) {
-            if (slopingPoint2 == startPoint || slopingPoint2 == slopingPoint) {
-                // don't observe current points
-                continue;
-            }
-
-            if (comparator.compare(slopingPoint, slopingPoint2) == 0) {
-                linkedPoints.addItem(slopingPoint2);
-            }
-        }
-
-        if (linkedPoints.count() >= 4) {
-            return createSegment(linkedPoints);
-        }
-
-        return null;
-    }
-
-    private LineSegment createSegment(LinkedList linkedList){
-        Object[] arr = linkedList.toArray();
-        Point[] points = new Point[arr.length];
-        for(int i = 0; i < arr.length; i++){
-            points[i] = (Point)arr[i];
-        }
-
-        Point min = points[0];
-        Point max = points[points.length - 1];
-
-        for(Point point: points){
-            if(point.compareTo(min) < 0){
-                min = point;
-            }
-            if(point.compareTo(max) > 0){
-                max = point;
-            }
-        }
-
-        return new LineSegment(min, max);
     }
 
     public int numberOfSegments() {
@@ -139,65 +104,16 @@ public class BruteCollinearPoints {
         return _segments;
     }
 
-    private class LinkedList<Item> {
-        private Node<Item> _first;
-        private Node<Item> _last;
+    private class Line {
+        public Point FirstPoint;
+        public Point LastPoint;
+        public double Slope;
 
-        public void addItem(Item item){
-            Node last = new Node(item);
-
-            if(_first == null){
-                _first = _last = last;
-                return;
-            }
-
-            _last.Next = last;
-            _last = last;
-        }
-
-        public int count(){
-            int count = 0;
-
-            Node current = _first;
-            while(current != null){
-                count++;
-                current = current.Next;
-            }
-
-            return count;
-        }
-
-        public Item first(){
-            return _first.Item;
-        }
-
-        public Item last(){
-            return _last.Item;
-        }
-
-        public Item[] toArray(){
-            int count = count();
-            Item[] items = (Item[])new Object[count];
-
-            Node current = _first;
-            int index = 0;
-            while(current != null){
-                items[index] = (Item)current.Item;
-
-                current = current.Next;
-                index++;
-            }
-
-            return items;
-        }
-    }
-
-    private class Node<Item>{
-        public Node Next;
-        public Item Item;
-
-        private Node(Item item){
-            Item = item;
+        public Line(Point firstPoint, Point lastPoint, double slope) {
+            FirstPoint = firstPoint;
+            LastPoint = lastPoint;
+            Slope = slope;
         }
     }
 }
+
